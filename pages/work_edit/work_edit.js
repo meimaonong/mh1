6d2d0b16b092641cd23d2a2378c156391c5790d0
覃超成 <meimaonong@163.com>
2017-11-10 12:41:35 +0800

    提交

--- pages/work_edit/work_edit.js
+++ pages/work_edit/work_edit.js
@@ -8,6 +8,8 @@ Page({
     isMask: true,
     array: ['美国', '中国', '巴西', '日本'],
     index: 0,
+    region: ['广东省', '广州市', '海珠区'],
+    customItem: '全部'
   },
   tap: function() {
 
@@ -73,4 +75,10 @@ Page({
       index: e.detail.value
     })
   },
+  bindRegionChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      region: e.detail.value
+    })
+  }
 })
