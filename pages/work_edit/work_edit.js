c5a35413d7607dde31b9422db97355a12efd0cde
覃超成 <meimaonong@163.com>
2017-11-10 11:48:33 +0800

    提交

--- pages/work_edit/work_edit.js
+++ pages/work_edit/work_edit.js
@@ -5,7 +5,9 @@ Page({
    * 页面的初始数据
    */
   data: {
-    isMask: true
+    isMask: true,
+    array: ['美国', '中国', '巴西', '日本'],
+    index: 0,
   },
   tap: function() {
 
@@ -64,5 +66,11 @@ Page({
    */
   onShareAppMessage: function () {
   
-  }
+  },
+  bindPickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
 })
