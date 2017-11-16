400ebb76e4f38727c199f93ccacc9e7990186406
覃超成 <meimaonong@163.com>
2017-11-16 19:19:57 +0800

    提交

--- pages/album/album.js
+++ pages/album/album.js
@@ -1,66 +1,37 @@
 // pages/album/album.js
+const params = require('../../utils/params')
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    albums: null
   },
+  getAlbums: function() {
+    var that = this
 
+    wx.request({
+      url: params.api + '/v1/album/get-albums',
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      data:{
+        user_id: 1
+      },
+      method: 'post',
+      success: function (res) {
+        that.setData({
+          albums: res.data.data
+        })
+
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var that = this
+    that.getAlbums()
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
   
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
