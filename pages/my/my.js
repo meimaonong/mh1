528fbf187d318b52068cacc245d6b2108779ff73
覃超成 <meimaonong@163.com>
2017-11-16 17:28:44 +0800

    提交

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,66 +1,48 @@
 // pages/my/my.js
+const params = require('../../utils/params')
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    wait_num: 0,
+    sell_num: 0,
+    buy_num: 0,
+    msg_num: 0,
+    album_num: 0,
   },
+  getNum: function() {
+    var that = this
+    wx.request({
+      url: params.api + '/v1/data/get-my-data',
+      method: 'post',
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      data: {
+        user_id: 1
+      },
+      success: function (res) {
+        var r = res.data.data
 
+        that.setData({
+          wait_num: parseInt(r.wait_num),
+          sell_num: parseInt(r.sell_num),
+          buy_num: parseInt(r.buy_num),
+          msg_num: parseInt(r.msg_num),
+          album_num: parseInt(r.album_num)
+        })
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var that = this
+    that.getNum()
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
