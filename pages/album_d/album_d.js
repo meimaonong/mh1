e0039fa1db62370a50090a794d82d7d912512f29
覃超成 <meimaonong@163.com>
2017-11-17 10:28:51 +0800

    提交

--- pages/album_d/album_d.js
+++ pages/album_d/album_d.js
@@ -1,21 +1,90 @@
 // pages/album_d/album_d.js
-Page({
+const params = require('../../utils/params')
 
+Page({
   /**
    * 页面的初始数据
    */
   data: {
-  
+    list: null,
+    isEdit: false,
+  },
+  toggle: function() {
+    var that = this
+    that.setData({
+      isEdit: !that.data.isEdit
+    })
+  },
+  del: function(e) {
+    var that = this
+    var index = e.currentTarget.dataset.index
+    var work_id = e.currentTarget.dataset.id
+    wx.showModal({
+      title: '确定删除？',
+      content: '',
+      success: function (res) {
+        if (res.confirm) {
+
+          wx.request({
+            url: params.api + '/v1/work/del-work',
+            header: {
+              'content-type': 'application/x-www-form-urlencoded'
+            },
+            data: {
+              user_id: 1,
+              work_id: work_id
+            },
+            method: 'post',
+            success: function (res) {
+              that.data.list.splice(index, 1)
+              that.setData({
+                list: that.data.list
+              })
+            }
+          })
+
+          
+        } else if (res.cancel) {
+          //console.log('用户点击取消')
+        }
+      }
+    })
+  },
+  edit: function (e) {
+    var that = this
+    var work_id = e.currentTarget.dataset.id
+    wx.navigateTo({
+      url: '/pages/work_edit/work_edit?work_id=' + work_id,
+    })
   },
   getWorks: function(album_id) {
+    var that = this
 
+    wx.request({
+      url: params.api + '/v1/work/get-worklist-by-album',
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      data: {
+        user_id: 1,
+        album_id: album_id
+      },
+      method: 'post',
+      success: function (res) {
+        that.setData({
+          list: res.data.data
+        })
+
+      }
+    })
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     var that = this
-    that.getWorks(options.album_id)
+    var album_id = options.album_id ? options.album_id : 1
+    that.getWorks(album_id)
   },
 
  
---
+++ pages/work_p/work_p.js
@@ -0,0 +1,39 @@
+
+const params = require('../../utils/params')
+
+Page({
+  data: {
+    work: null
+  },
+  getWork: function (work_id) {
+    var that = this
+    wx.request({
+      url: params.api + '/v1/work/get-work',
+      method: 'post',
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      data: {
+        work_id: work_id
+      },
+      success: function (res) {
+        var data = res.data.data
+        data.workItems.map(function (item) {
+          item['style'] = 'height:' + (690 / parseFloat(item.ratio)) + 'rpx'
+        })
+        console.log(data)
+        that.setData({
+          work: data
+        })
+      }
+    })
+  },
+  onLoad: function (options) {
+    var that = this
+    console.log(options)
+    var work_id = options.work_id ? options.work_id : 1
+    that.getWork(work_id)
+
+  }
+
+})
